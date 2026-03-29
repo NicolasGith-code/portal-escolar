e77c9b57d2ffe746b41e6081bb714ee79cde7c5f
NicolasGith-code <nicolaspizarro1306@gmail.com>
2026-03-29 16:42:46 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 
+function login() {
+    let usuario = document.getElementById("usuario").value;
+    let password = document.getElementById("password").value;
+
+    // Login de prueba
+    if(usuario === "alumno" && password === "1234"){
+        window.location.href = "dashboard-alumno.html";
+    } else {
+        alert("Usuario o contraseña incorrectos");
+    }
+}
