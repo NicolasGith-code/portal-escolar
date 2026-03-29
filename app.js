84cdd1c820ab3060d26acccdb5708650c36fde02
NicolasGith-code <nicolaspizarro1306@gmail.com>
2026-03-29 16:46:58 -0300

    Update app.js
    Función por materia

--- app.js
+++ app.js
@@ -10,3 +10,38 @@ function login() {
         alert("Usuario o contraseña incorrectos");
     }
 }
+function irNotas(){
+    window.location.href = "notas.html";
+}
+
+function volver(){
+    window.location.href = "dashboard-alumno.html";
+}
+
+function cargarNotas(){
+    const notas = [
+        {asignatura: "Matemáticas", nota: 6.5, fecha: "10-03-2026"},
+        {asignatura: "Lenguaje", nota: 5.8, fecha: "12-03-2026"},
+        {asignatura: "Historia", nota: 6.2, fecha: "15-03-2026"},
+        {asignatura: "Ciencias", nota: 6.9, fecha: "18-03-2026"}
+    ];
+
+    let tabla = document.getElementById("tablaNotas");
+
+    notas.forEach(nota => {
+        let fila = `
+            <tr>
+                <td>${nota.asignatura}</td>
+                <td>${nota.nota}</td>
+                <td>${nota.fecha}</td>
+            </tr>
+        `;
+        tabla.innerHTML += fila;
+    });
+}
+
+window.onload = function(){
+    if(document.getElementById("tablaNotas")){
+        cargarNotas();
+    }
+}
